f132a6352cd8f3981027e4d9d286e05840d88483
gerardonastri <gerardonastri06@gmail.com>
2022-05-30 14:58:04 +0200

    better

--- util/apiCalls.js
+++ util/apiCalls.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const  BASE_URL = "http://twitter-clone.vercel.app/api/";
+const  BASE_URL = "https://twitter-clone-flax-eight.vercel.app/api/";
 
 const axiosReq = axios.create({
     baseURL:  BASE_URL
